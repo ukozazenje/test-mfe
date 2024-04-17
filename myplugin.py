b3ef404410cbdb441c0f48001bc383f5aa52a04d
mzecevic04 <mzecevic04@gmail.com>
2024-04-18 00:40:07 +0200

    feat: test app

--- myplugin.py
+++
@@ -1,9 +0,0 @@
-from tutormfe.hooks import MFE_APPS
-
-@MFE_APPS.add()
-def _add_my_mfe(mfes):
-    mfes["mymfe"] = {
-        "repository": "https://github.com/ukozazenje/test-mfe.git",
-        "port": 1995,# optional, will default to the Open edX current tag.
-    }
-    return mfes
